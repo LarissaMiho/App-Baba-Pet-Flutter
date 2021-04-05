cf3457f3d61b874d9a1ca0646cf9184aa4ecb9a7
Miho <mihojp123@gmail.com>
2021-04-05 16:32:47 -0300

    primeiro commit

---
+++ lib/telaInicial.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:primeiro/escolhaUsuario.dart';
+import 'package:primeiro/listaBabas.dart';
+import 'package:primeiro/listaPedidos.dart';
+import 'package:primeiro/termosServicos.dart';
+
+class TelaInicial extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Babá Pet"),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+         mainAxisSize: MainAxisSize.min,
+         children: <Widget>[
+           RaisedButton(
+            onPressed: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(
+                  builder: (BuildContext context) => ListaBabas()));
+            },
+            child: Text("Entrar com Facebook", style: TextStyle(
+              fontSize: 18,
+              color: Colors.white,
+            ),),
+            color: Colors.indigo[900],
+            
+            ),
+            
+            
+            RaisedButton(
+            onPressed: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(
+                  builder: (BuildContext context) => ListaPedidos()));
+            },
+            child: Text("Entrar com Instagram", style: TextStyle(
+              fontSize: 18,
+              color: Colors.white,
+            ),),
+            color: Colors.deepPurple[400],
+            ),
+
+            Text('', 
+    style: TextStyle(
+      fontSize: 15, 
+    )),//////////////////////////////////////////////////////////////
+
+    Text('', 
+    style: TextStyle(
+      fontSize: 15, 
+    )),//////////////////////////////////////////////////////////////
+
+      
+            RaisedButton(
+            onPressed: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(
+                  builder: (BuildContext context) => EscolhaUsuario()));
+            },
+            child: Text("Cadastrar-se", style: TextStyle(
+              fontSize: 15,
+              color: Colors.white,
+            ),),
+            color: Colors.brown,
+            ),
+             Text('', 
+    style: TextStyle(
+      fontSize: 15, 
+    )),
+     Text('', 
+    style: TextStyle(
+      fontSize: 15, 
+    )),
+    Text('', 
+    style: TextStyle(
+      fontSize: 15, 
+    )),
+    Text('', 
+    style: TextStyle(
+      fontSize: 15, 
+    )),
+    Text('', 
+    style: TextStyle(
+      fontSize: 15, 
+    )),
+    Text('', 
+    style: TextStyle(
+      fontSize: 15, 
+    )),
+    Text('', 
+    style: TextStyle(
+      fontSize: 15, 
+    )),
+
+    Text('', 
+    style: TextStyle(
+      fontSize: 15, 
+    )),
+    Text('', 
+    style: TextStyle(
+      fontSize: 15, 
+    )),
+    Text('', 
+    style: TextStyle(
+      fontSize: 15, 
+    )),
+    Text('', 
+    style: TextStyle(
+      fontSize: 15, 
+    )),
+    Text('', 
+    style: TextStyle(
+      fontSize: 15, 
+    )),
+    Text('', 
+    style: TextStyle(
+      fontSize: 15, 
+    )),
+
+
+            RaisedButton(
+            onPressed: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(
+                  builder: (BuildContext context) => TermosServicos()));
+            },
+            child: Text("Termos de Serviço", style: TextStyle(
+              fontSize: 15,
+            ),),
+            color: Colors.white,
+            ),
+
+  ],
+)
+        ),
+      ),
+      
+    );
+  }
+}
